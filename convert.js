90ac9a000b7f28b4b3713e33081b2a2ed459f517
harryangstrom <harry.angstrom67@gmail.com>
2018-12-16 20:07:21 +0100

    1st commit

---
+++ convert.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const csv2json = require('csvtojson');
+
+//Main function: it gets the file to be converted and write a json file with the output.
+//Everything is treated async to not block the processes.
+const convert = (file) => {
+  if (!file) return console.log('Usage: node convert filename [output]'); //if the file does not exist, it prints usage
+  const thisfile = path.join(__dirname, file); //path_to_csv_file
+  const thatfile = path.join(__dirname, path.basename(thisfile, '.csv') + '.json'); //path_to_json_file
+  csv2json()
+    .fromFile(thisfile)
+    .then((jsonObj) => {
+      console.log(jsonObj);
+      str = JSON.stringify(jsonObj); //convert the JSON object to string
+      //create and write the json file with the content.
+      fs.writeFile(thatfile, str, (err) => {
+        if (err) throw err;
+        console.log("JSON succesfully converted");
+      });
+    })
+}
+
+//entry point to the utility.
+convert(process.argv[2]);
